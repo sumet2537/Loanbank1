283413cbb5ac0cdb9622f45a74ce96b030c2451c
Sumet Kaenkaew <sumat68@hotmail.com>
2016-12-22 21:50:49 +0700

    22/122

--- src/java/com/service/LoanService.java
+++ src/java/com/service/LoanService.java
@@ -28,6 +28,25 @@ public class LoanService {
             fileTransferer.upload(requestLoanBean.getCopymarriage(), requestLoanBean.getCopymarriagebyte());
             fileTransferer.upload(requestLoanBean.getCopy_rename(), requestLoanBean.getCopy_renamebyte());
             fileTransferer.upload(requestLoanBean.getSalary_slip(), requestLoanBean.getSalary_slipbyte());
+            fileTransferer.upload(requestLoanBean.getCopy_bankaccount(), requestLoanBean.getCopy_bankaccountbyte());
+
+//            fileTransferer.upload(requestLoanBean.getCopyidcard_p(), requestLoanBean.getCopyidcardbyte());
+//            fileTransferer.upload(requestLoanBean.getCopydocumenthome_p(), requestLoanBean.getCopydocumenthome_pbyte());
+//            fileTransferer.upload(requestLoanBean.getSalary_slip_p(), requestLoanBean.getSalary_slip_pbyte());
+//            fileTransferer.upload(requestLoanBean.getCopy_bankaccount_p(), requestLoanBean.getCopy_bankaccountbyte());
+            if ("สมรส".equals(requestLoanBean.getStatustype())) {
+                fileTransferer.upload(requestLoanBean.getCopyidcard_p(), requestLoanBean.getCopyidcardbyte());
+                fileTransferer.upload(requestLoanBean.getCopydocumenthome_p(), requestLoanBean.getCopydocumenthome_pbyte());
+                fileTransferer.upload(requestLoanBean.getSalary_slip_p(), requestLoanBean.getSalary_slip_pbyte());
+                fileTransferer.upload(requestLoanBean.getCopy_bankaccount_p(), requestLoanBean.getCopy_bankaccountbyte());
+            } else {
+
+            }
+            fileTransferer.upload(requestLoanBean.getCopyidcard_k(), requestLoanBean.getCopyidcard_kv());
+            fileTransferer.upload(requestLoanBean.getCopydocumenthome_k(), requestLoanBean.getCopydocumenthome_kv());
+            fileTransferer.upload(requestLoanBean.getSalary_slip_k(), requestLoanBean.getSalary_slip_kbyte());
+            fileTransferer.upload(requestLoanBean.getCopy_bankaccount_k(), requestLoanBean.getCopy_bankaccount_kbyte());
+
             result = dao.insertbank(requestLoanBean);
         } catch (Exception ex) {
             Logger.getLogger(com.ws.RequestLoanService.class.getName()).log(Level.SEVERE, null, ex);
